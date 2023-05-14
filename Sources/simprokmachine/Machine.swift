88443b5bfd913666e2d674418be7d0f6480141a4
prokhor-andrew <prokhor.andrew@gmail.com>
2023-05-13 21:31:27 -0400

    Added isConsumeOnMain flag to subscribe function

--- Sources/simprokmachine/Machine.swift
+++ Sources/simprokmachine/Machine.swift
@@ -5,35 +5,31 @@
 //  Created by Andrey Prokhorenko on 01.01.2020.
 //  Copyright (c) 2020 simprok. All rights reserved.
 
-import Foundation
+
 
 public struct Machine<Input, Output> {
 
     internal let id: ObjectIdentifier
     internal let onProcess: BiHandler<Input?, Handler<Output>>
-    internal let processQueue: DispatchQueue
-    internal let outputQueue: DispatchQueue
-
+    internal let isProcessOnMain: Bool
+    
     public init<Object: AnyObject>(
             _ object: Object,
             isProcessOnMain: Bool = false,
             onProcess: @escaping TriHandler<Object, Input?, Handler<Output>>
     ) {
-        func queue(tag: String) -> DispatchQueue {
-            DispatchQueue(label: String(describing: Self.self) + "/" + tag, qos: .userInteractive)
-        }
-
-        processQueue = isProcessOnMain ? .main : queue(tag: "process")
-        outputQueue = queue(tag: "output")
-
         id = ObjectIdentifier(object)
+        self.isProcessOnMain = isProcessOnMain
         self.onProcess = { [object] input, callback in
             onProcess(object, input, callback)
         }
     }
 
-    public func subscribe(_ callback: @escaping BiHandler<Output, Handler<Input>>) -> Subscription<Input, Output> {
-        Subscription(machine: self, callback: callback)
+    public func subscribe(
+        isConsumeOnMain: Bool = false,
+        callback: @escaping BiHandler<Output, Handler<Input>>
+    ) -> Subscription<Input, Output> {
+        Subscription(machine: self, isConsumeOnMain: isConsumeOnMain, callback: callback)
     }
 }
 
@@ -53,8 +49,9 @@ public extension Machine {
     private class Dummy {
     }
 
-    init(isProcessOnMain: Bool = false,
-         onProcess: @escaping BiHandler<Input?, Handler<Output>>
+    init(
+        isProcessOnMain: Bool = false,
+        onProcess: @escaping BiHandler<Input?, Handler<Output>>
     ) {
         self.init(Dummy(), isProcessOnMain: isProcessOnMain, onProcess: { _, input, callback in
             onProcess(input, callback)
--- Sources/simprokmachine/Subscription.swift
+++ Sources/simprokmachine/Subscription.swift
@@ -6,19 +6,31 @@
 //  Copyright (c) 2020 simprok. All rights reserved.
 
 
+import Foundation
+
 public final class Subscription<Input, Output> {
 
     private let machine: Machine<Input, Output>
     private let callback: BiHandler<Output, Handler<Input>>
+    private let processQueue: DispatchQueue
+    private let outputQueue: DispatchQueue
 
     internal init(
             machine: Machine<Input, Output>,
+            isConsumeOnMain: Bool,
             callback: @escaping BiHandler<Output, Handler<Input>>
     ) {
+        func queue(tag: String) -> DispatchQueue {
+            DispatchQueue(label: String(describing: Self.self) + "/" + tag, qos: .userInteractive)
+        }
+        
         // we check that this machine has not been subscribed to
         assert(!subscribed.contains(machine.id), "Machine already subscribed")
         subscribed.insert(machine.id)
 
+        self.processQueue = machine.isProcessOnMain ? .main : queue(tag: "process")
+        self.outputQueue = isConsumeOnMain ? .main : queue(tag: "output")
+        
         self.machine = machine
         self.callback = callback
 
@@ -34,9 +46,9 @@ public final class Subscription<Input, Output> {
     }
 
     private func _send(_ input: Input?) {
-        machine.processQueue.async { [weak self] in
+        processQueue.async { [weak self] in
             self?.machine.onProcess(input) { [weak self] output in
-                self?.machine.outputQueue.async { [weak self] in
+                self?.outputQueue.async { [weak self] in
                     guard let send = self?.send else {
                         return
                     }
