2fadf2527e303223fefc9fba819884a0fed1c893
prokhor-andrew <prokhor.andrew@gmail.com>
2023-07-08 23:45:26 -0400

    Added async functions

--- Sources/simprokmachine/Functions.swift
+++ Sources/simprokmachine/Functions.swift
@@ -16,3 +16,15 @@ public typealias BiMapper<T1, T2, R> = (T1, T2) -> R
 public typealias TriMapper<T1, T2, T3, R> = (T1, T2, T3) -> R
 
 public typealias Supplier<T> = () -> T
+
+
+public typealias AsyncAction = () async -> Void
+public typealias AsyncHandler<T> = (T) async -> Void
+public typealias AsyncBiHandler<T1, T2> = (T1, T2) async -> Void
+public typealias AsyncTriHandler<T1, T2, T3> = (T1, T2, T3) async -> Void
+
+public typealias AsyncMapper<T, R> = (T) async -> R
+public typealias AsyncBiMapper<T1, T2, R> = (T1, T2) async -> R
+public typealias AsyncTriMapper<T1, T2, T3, R> = (T1, T2, T3) async -> R
+
+public typealias AsyncSupplier<T> = () async -> T
