8d7bd6f9f72381911647a1af22ea9034021cdecf
prokhor-andrew <prokhor.andrew@gmail.com>
2023-07-08 23:33:28 -0400

    Added functions

---
+++ Sources/simprokmachine/Functions.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Andriy Prokhorenko on 08.07.2023.
+//
+
+
+public typealias Action = () -> Void
+public typealias Handler<T> = (T) -> Void
+public typealias BiHandler<T1, T2> = (T1, T2) -> Void
+public typealias TriHandler<T1, T2, T3> = (T1, T2, T3) -> Void
+
+public typealias Mapper<T, R> = (T) -> R
+public typealias BiMapper<T1, T2, R> = (T1, T2) -> R
+public typealias TriMapper<T1, T2, T3, R> = (T1, T2, T3) -> R
+
+public typealias Supplier<T> = () -> T
