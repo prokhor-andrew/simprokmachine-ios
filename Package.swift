6e0724601c19b5e12e721110f8b1425b2418148c
prokhor-andrew <prokhor.andrew@gmail.com>
2023-07-12 10:16:30 -0400

    limitation to iOS 13 added into Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "simprokmachine",
-    platforms: [.macOS(.v10_15)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "simprokmachine",
